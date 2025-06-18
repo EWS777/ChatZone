e9050f58e8f7c6c50d2cc25e4e76a546b035dc73
EWS777 <egor.vasilenko2004@gmail.com>
2025-06-18 16:22:07 +0200

    feat: added create blocked person controller

---
+++ ChatZone.Api/Features/BlockedPersons/Create/CreateBlockedPersonRequest.cs
@@ -0,0 +1,11 @@
+﻿using ChatZone.Core.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatZone.Features.BlockedPersons.Create;
+
+public class CreateBlockedPersonRequest : IRequest<Result<IActionResult>>
+{
+    public int IdPerson { get; init; }
+    public int IdBlockedPerson { get; init; }
+}
