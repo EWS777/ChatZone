94126322a7cc4863030e1ed7f33adebd384f6595
EWS777 <egor.vasilenko2004@gmail.com>
2025-07-17 21:21:00 +0200

    feat: add match persons again logic

--- ChatZone.Api/Matchmaking/MatchmakingService.cs
+++ ChatZone.Api/Matchmaking/MatchmakingService.cs
@@ -23,6 +23,19 @@ public class MatchmakingService(ChatZoneDbContext dbContext) : IMatchmakingServi
             Language = request.Language
         };
         
+        if (request.IsSearchAgain)
+        {
+            var firstPersonAgain = await dbContext.PersonFilterProperties.SingleOrDefaultAsync(x => x.IdPerson == request.IdPerson, cancellationToken);
+            
+            firstPerson.Theme = firstPersonAgain!.Theme;
+            firstPerson.Country = firstPersonAgain.Country;
+            firstPerson.City = firstPersonAgain.City;
+            firstPerson.Age = firstPersonAgain.Age;
+            firstPerson.YourGender = firstPersonAgain.YourGender;
+            firstPerson.PartnerGender = firstPersonAgain.PartnerGender;
+            firstPerson.Language = firstPersonAgain.Language;
+        }
+        
         var secondPerson = await dbContext.MatchQueues.FirstOrDefaultAsync(x=> x.IdPerson != firstPerson.IdPerson, cancellationToken);
         if (secondPerson == null)
         {
@@ -39,10 +52,35 @@ public class MatchmakingService(ChatZoneDbContext dbContext) : IMatchmakingServi
             CreatedAt = DateTimeOffset.UtcNow
         };
         
+        if (!request.IsSearchAgain)
+        {
+            var isPersonExists = await dbContext.PersonFilterProperties.AnyAsync(x => x.IdPerson == firstPerson.IdPerson, cancellationToken);
+            if (!isPersonExists)
+            {
+                await AddFilterPropertiesAsync(firstPerson, cancellationToken);
+                await AddFilterPropertiesAsync(secondPerson, cancellationToken);
+            }
+        }
+        
         dbContext.MatchQueues.Remove(secondPerson);
         await dbContext.SingleChats.AddAsync(chat, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
         
         return (firstPerson, secondPerson, chat.IdSingleChat.ToString());
+    }
+
+    private async Task AddFilterPropertiesAsync(MatchQueue person, CancellationToken cancellationToken)
+    { 
+        await dbContext.PersonFilterProperties.AddAsync(new PersonFilterProperty 
+        {
+            IdPerson = person.IdPerson,
+            Theme = person.Theme,
+            Country = person.Country,
+            City = person.City,
+            Age = person.Age,
+            YourGender = person.YourGender,
+            PartnerGender = person.PartnerGender,
+            Language = person.Language
+        },cancellationToken);
     }
 }
