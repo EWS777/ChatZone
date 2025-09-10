368c159a3d2652267d90163b553cf9131558bb85
EWS777 <egor.vasilenko2004@gmail.com>
2025-09-10 21:43:21 +0200

    feat: implement method to check if user sent any messages in group

--- ChatZone.Api/Chat/ChatHub.cs
+++ ChatZone.Api/Chat/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using ChatZone.Core.Services;
 using ChatZone.Features.Messages.Add;
+using ChatZone.Features.Messages.GetStatus;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
 
@@ -41,7 +42,7 @@ public class ChatHub(IMediator mediator) : Hub
     {
         return Context.ConnectionId;
     }
-    public object GetPersonGroupAndUsername()
+    public async Task<object> GetPersonGroupAndUsername()
     {
         var personId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         if (personId is null) throw new Exception("User does not exist!");
@@ -55,7 +56,10 @@ public class ChatHub(IMediator mediator) : Hub
         var idPerson = int.Parse(personId);
         var idPartnerPerson = ChatManagerService.GetSecondPersonInSingleChat(idGroup, int.Parse(personId));
         
-        return new { idPerson, idGroup, isSingleChat, isPartnerIdPeron = isSingleChat ? idPartnerPerson : (int?)null};
+        var isSentMessage = await mediator.Send(new GetStatusRequest { IdPerson = int.Parse(personId), IdChat = idGroup.Value, IsSingleChat = isSingleChat });
+        if (idGroup == 0) idGroup = null;
+        
+        return new { idPerson, idGroup, isSingleChat, isPartnerIdPeron = isSingleChat ? idPartnerPerson : (int?)null, isSentMessage};
     }
     
     public async Task LeaveChat(int idGroup, bool isSingleChat)
---
+++ ChatZone.Api/Features/Messages/GetStatus/GetStatusHandler.cs
@@ -0,0 +1,18 @@
+﻿using ChatZone.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatZone.Features.Messages.GetStatus;
+
+public class GetStatusHandler(
+    ChatZoneDbContext dbContext) : IRequestHandler<GetStatusRequest, bool>
+{
+    public async Task<bool> Handle(GetStatusRequest request, CancellationToken cancellationToken)
+    {
+        var result = await (request.IsSingleChat 
+            ? dbContext.SingleMessages.AnyAsync(x => x.IdChat == request.IdChat && x.IdSender == request.IdPerson, cancellationToken) 
+            : dbContext.GroupMessages.AnyAsync(x => x.IdChat == request.IdChat && x.IdSender == request.IdPerson, cancellationToken));
+        
+        return result;
+    }
+}
