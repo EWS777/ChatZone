4598238096749258a09ec61eb84f2eac1b451e92
EWS777 <egor.vasilenko2004@gmail.com>
2025-09-12 22:51:20 +0200

    feat: implement get chat person info endpoint with logic

---
+++ ChatZone.Api/Features/ChatInfo/ChatInfoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using ChatZone.Features.ChatInfo.Get;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatZone.Features.ChatInfo;
+
+[ApiController]
+[Route("[controller]")]
+public class ChatInfoController(IMediator mediator) : ControllerBase
+{
+    [Authorize(Roles = "User")]
+    [HttpGet]
+    [Route("")]
+    public async Task<GetChatPersonInfoResponse> GetChatPersonInfo(CancellationToken cancellationToken)
+    {
+        var idPerson = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (idPerson is null) throw new Exception("User does not exist!");
+        var result = await mediator.Send(new GetChatPersonInfoRequest{IdPerson = int.Parse(idPerson)}, cancellationToken);
+        return result.Match<GetChatPersonInfoResponse>(e => e, x=> throw x);
+    }
+}
---
+++ ChatZone.Api/Features/ChatInfo/Get/GetChatPersonInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatZone.Features.ChatInfo.Get;
+
+public class GetChatPersonInfoResponse
+{
+    public int IdPerson { get; set; }
+    public int? IdGroup { get; set; }
+    public bool? IsSingleChat { get; set; }
+    public int? IdPartnerPerson { get; set; }
+    public bool? IsSentMessage { get; set; }
+}
