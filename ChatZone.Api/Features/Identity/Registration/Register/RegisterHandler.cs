53f1df206d62074b910de4f0362ec5ffafa0fff0
EWS777 <egor.vasilenko2004@gmail.com>
2025-11-11 19:08:07 +0100

    refactor: remove unnecessary try-catch block

--- ChatZone.Api/Features/Identity/Registration/Register/RegisterHandler.cs
+++ ChatZone.Api/Features/Identity/Registration/Register/RegisterHandler.cs
@@ -17,40 +17,32 @@ public class RegisterHandler(
 {
     public async Task<Result<IActionResult>> Handle(RegisterRequest request, CancellationToken cancellationToken)
     {
-        Person person;
-        try
-        {
-            var getEmailResult = await dbContext.Persons.AnyAsync(x=>x.Email == request.Email, cancellationToken);
+        var getEmailResult = await dbContext.Persons.AnyAsync(x=>x.Email == request.Email, cancellationToken);
 
-            if (getEmailResult) return Result<IActionResult>.Failure(new ExistPersonException("The email or username already exists."));
-            
-            var getUsernameResult = await dbContext.Persons.AnyAsync(x=>x.Username == request.Username, cancellationToken);
-            
-            if (getUsernameResult) return Result<IActionResult>.Failure(new ExistPersonException("The email or username already exists."));
-            
-            var getHashedPasswordAndSalt = SecurityHelper.GetHashedPasswordAndSalt(request.Password);
-            
-            person = new Person
-            {
-                Role = PersonRole.Unconfirmed,
-                Username = request.Username,
-                Email = request.Email,
-                Password = getHashedPasswordAndSalt.Item1,
-                Salt = getHashedPasswordAndSalt.Item2,
-                RefreshToken = SecurityHelper.GenerateRefreshToken(),
-                RefreshTokenExp = DateTimeOffset.UtcNow.AddDays(7),
-                EmailConfirmToken = token.GenerateAuthorizationToken(),
-                EmailConfirmTokenExp = DateTimeOffset.UtcNow.AddMinutes(15)
-            };
-            
-            await dbContext.Persons.AddAsync(person, cancellationToken);
-            await dbContext.SaveChangesAsync(cancellationToken);
-        }
-        catch (Exception e)
-        {
-            return Result<IActionResult>.Failure(new BackendException());
-        }
+        if (getEmailResult) return Result<IActionResult>.Failure(new ExistPersonException("The email or username already exists."));
         
+        var getUsernameResult = await dbContext.Persons.AnyAsync(x=>x.Username == request.Username, cancellationToken);
+        
+        if (getUsernameResult) return Result<IActionResult>.Failure(new ExistPersonException("The email or username already exists."));
+        
+        var getHashedPasswordAndSalt = SecurityHelper.GetHashedPasswordAndSalt(request.Password);
+        
+        var person = new Person
+        {
+            Role = PersonRole.Unconfirmed,
+            Username = request.Username,
+            Email = request.Email,
+            Password = getHashedPasswordAndSalt.Item1,
+            Salt = getHashedPasswordAndSalt.Item2,
+            RefreshToken = SecurityHelper.GenerateRefreshToken(),
+            RefreshTokenExp = DateTimeOffset.UtcNow.AddDays(7),
+            EmailConfirmToken = token.GenerateAuthorizationToken(),
+            EmailConfirmTokenExp = DateTimeOffset.UtcNow.AddMinutes(15)
+        };
+        
+        await dbContext.Persons.AddAsync(person, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
         await EmailSender.SendCodeToEmail(person.Email, person.EmailConfirmToken, cancellationToken);
         
         return Result<IActionResult>.Ok(new OkResult());
