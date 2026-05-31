57e22f4707629869b95d6da58b8efbfb990e5cdc
EWS777 <egor.vasilenko2004@gmail.com>
2026-05-31 22:28:11 +0200

    fix: change token convertor

--- ChatZone.Api/Shared/Security/SecurityHelper.cs
+++ ChatZone.Api/Shared/Security/SecurityHelper.cs
@@ -44,7 +44,7 @@ public static class SecurityHelper
         using (var rng = RandomNumberGenerator.Create())
         {
             rng.GetBytes(randomNumber);
-            return Convert.ToBase64String(randomNumber);
+            return Convert.ToHexString(randomNumber);
         }
     }
 
@@ -53,6 +53,6 @@ public static class SecurityHelper
         using var sha256 = SHA256.Create();
         var bytes = Encoding.UTF8.GetBytes(token);
         var hash = sha256.ComputeHash(bytes);
-        return Convert.ToBase64String(hash);
+        return Convert.ToHexString(hash);
     }
 }
